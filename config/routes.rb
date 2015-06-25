3ce90103acb64a279c004386504fd67b27aa8a79
JimT2 <jtunnessen@gmail.com>
2015-06-24 22:32:27 -0400

    Added Navbar, CSS padding, and filled in the <head> data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :video_lessons
-  root 'home#index'
+  root 'video_lessons#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
